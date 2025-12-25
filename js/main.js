fe4e8a2e2c269c9652e936919053a35a97148a89
Uyanda <128975995+uyanda-maseti@users.noreply.github.com>
2025-12-25 17:11:18 +0000

    working with arrays

--- js/main.js
+++ js/main.js
@@ -12,3 +12,31 @@ function boolToWord( bool ){
   //...
   return bool ? 'Yes' : 'No'
 }
+
+let arr = [1,2,3]
+
+
+function getLength(arr){
+  //return length of arr
+  return arr.length
+}
+function getFirst(arr){
+  //return the first element of arr
+  return arr[0]
+}
+function getLast(arr){
+  //return the last element of arr
+  return arr[arr.length-1]
+}
+function pushElement(arr){
+  var el=1;
+  //push el to arr
+  
+  return arr.push(el)
+}
+function popElement(arr){
+  //pop an element from arr
+  
+  return arr.pop()
+}
+
