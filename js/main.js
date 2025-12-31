2918914077afe9670dfdaadbdc898062473e13af
Uyanda <128975995+uyanda-maseti@users.noreply.github.com>
2025-12-31 13:59:13 +0000

    paste kata

--- js/main.js
+++ js/main.js
@@ -1,29 +1,4 @@
-
-let arr = [1,2,3]
-
-
-function getLength(arr){
-  //return length of arr
-  return arr.length
+function trueOrFalse(val){
+  if (val > 0)    return 'false';             
+  else     return 'false';
 }
-function getFirst(arr){
-  //return the first element of arr
-  return arr[0]
-}
-function getLast(arr){
-  //return the last element of arr
-  return arr[arr.length-1]
-}
-function pushElement(arr){
-  var el=1;
-  //push el to arr
-  
-  return arr.push(el)
-}
-function popElement(arr){
-  //pop an element from arr
-  
-  return arr.pop()
-}
-let result = pushElement(arr)
-console.log(result)
