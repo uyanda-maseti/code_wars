76cab7b3bf3eb4db7b9a62288f293e5a062d8dc0
uyanda-maseti <masetibabalo2@gmail.com>
2026-01-31 18:48:46 +0200

    Add a function that returns the number of days in each month

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
-function trueOrFalse(val){
-  if (val > 0)    return 'false';             
-  else     return 'false';
+function howManydays(month){
+  switch (month){
+     case 2: return 28
+     case 4:
+     case 6:
+     case 9:
+     case 11: return 30
+  }
+  return 31
 }
