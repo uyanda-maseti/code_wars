abac02a351336c024615ae625093dc413354b9f4
Uyanda <128975995+uyanda-maseti@users.noreply.github.com>
2026-02-13 13:39:06 +0000

    we must push code everyday

--- js/main.js
+++ js/main.js
@@ -1,18 +1,2 @@
-const dolls =["Mickey Mouse", "Hello Kitty", "Hello Kitty", "Barbie doll", "Snow white"]
+console.log('Hello, world!')
 
-function grabDoll(dolls){
-	  var bag=[];
-	  //coding here
-	for(let i = 0; i < dolls.length; i++) {
-		if (dolls[i] === "Hello Kitty" || dolls[i] === "Barbie doll") {
-			bag.push(dolls[i])
-		} else {
-
-			continue
-		}
-
-	}
-	  
-	    return bag;
-}
-console.log(grabDoll(dolls))
